17b024d0b895fdc2cb2e5488939ba88f190d96dc
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-06-30 00:01:06 +0200

    Started adding changelog-checking code

---
+++ changebot/changelog.py
@@ -0,0 +1,113 @@
+import re
+import base64
+import requests
+
+
+BLOCK_PATTERN = re.compile('\[#.+\]', flags=re.DOTALL)
+ISSUE_PATTERN = re.compile('#[0-9]+')
+
+
+def find_prs_in_changelog(content):
+    issue_numbers = []
+    for block in BLOCK_PATTERN.finditer(content):
+        block_start, block_end = block.start(), block.end()
+        block = content[block_start:block_end]
+        for m in ISSUE_PATTERN.finditer(block):
+            start, end = m.start(), m.end()
+            issue_numbers.append(int(block[start:end][1:]))
+    return issue_numbers
+
+
+def find_prs_in_changelog_by_section(content):
+
+    changelog_prs = {}
+    version = None
+    subcontent = ''
+    previous = None
+
+    for line in content.splitlines():
+        if '-------' in line:
+            if version is not None:
+                for pr in find_prs_in_changelog(subcontent):
+                    changelog_prs[int(pr)] = version
+            version = previous.strip().split('(')[0].strip()
+            if 'v' not in version:
+                version = 'v' + version
+            subcontent = ''
+        elif version is not None:
+            subcontent += line
+        previous = line
+
+    return changelog_prs
+
+
+def check_changelog_consistency(webhook_payload, headers):
+
+    # Get pull request number
+    pull_request = webhook_payload['number']
+
+    # Figure out the URL to the changelog file for the PR head
+    url_changes = webhook_payload['contents_url'].replace('{+path}', 'CHANGES.rst')
+
+    # Make sure we get the changes from the same branch.
+    data = {}
+    data['ref'] = data['head']['ref']
+
+    # Get the contents of the changelog file
+    response = requests.get(url_changes, params=data, headers=headers)
+    changelog_base64 = response.json()['content']
+
+    # Decode from base64
+    changelog = base64.b64decode(changelog_base64)
+
+    # Next, we need to get the milestone of the PR
+    milestone = webhook_payload['milestone']['title']
+
+    # Finally, we need to get the labels
+    response = requests.get(webhook_payload['milestone']['labels_url'], headers=headers)
+    labels = [label['name'] for label in response.json()]
+
+    status, message = review_changelog(pull_request, changelog, milestone, labels)
+
+
+def review_changelog(pull_request, changelog, milestone, labels):
+
+    issues = []
+
+    if milestone is None:
+        issues.append("The milestone has not been set")
+
+    sections = find_prs_in_changelog_by_section(changelog)
+    changelog_entry = pull_request in sections
+    if changelog_entry:
+        if not milestone.startswith(sections[pull_request]):
+            issues.append("Changelog entry section ({0}) inconsistent with milestone ({1})".format(sections[pull_request], milestone))
+
+    if 'no-changelog-entry-needed' in labels:
+        if changelog_entry:
+            issues.append("Changelog entry present but **no-changelog-entry-needed** label set")
+    elif 'Affects-dev' in labels:
+        if changelog_entry:
+            issues.append("Changelog entry present but **Affects-dev** label set")
+    else:
+        if not changelog_entry:
+            issues.append("Changelog entry not present (or pull request number missing) and neither the **Affects-dev** nor the **no-changelog-entry-needed** label are set")
+
+    if len(issues) > 0:
+
+        message = "@{0} - thanks for merging this! However, I noticed the following issues with this pull request:\n\n".format(merged_by)
+        for issue in issues:
+            message += "* {0}\n".format(issue)
+
+        message += "\nWould it be possible to fix these? Thanks! \n"
+
+        if len(issues) == 1:
+            message = message.replace('issues with', 'issue with').replace('fix these', 'fix this')
+
+        message += "\n*If you believe the above to be incorrect (which I - @astrobot - very much doubt) you can ping @astrofrog*\n"
+
+        return False, message
+
+    else:
+
+        return True, "All good!"
--- changebot/webapp.py
+++ changebot/webapp.py
@@ -1,15 +1,18 @@
 import os
 import json
+import datetime
 
-from flask import Flask, redirect, url_for, request
+import jwt
 
-from github import Github
-from github.GithubException import GithubException
+import requests
+
+from flask import Flask, request
 
 from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask('astrochangebot')
 app.wsgi_app = ProxyFix(app.wsgi_app)
+app.integration_id = int(os.environ['GITHUB_APP_INTEGRATION_ID'])
 app.private_key = os.environ['GITHUB_APP_PRIVATE_KEY']
 
 
@@ -23,30 +26,87 @@ def installation_authorized():
     return "Nothing to see here"
 
 
+def get_jwt():
+    """
+    Prepares the JSON Web Token (JWT) based on the private key.
+    """
+
+    now = datetime.datetime.now()
+    expiration_time = now + datetime.timedelta(minutes=9)
+    payload = {
+        # Issued at time
+        'iat': int(now.timestamp()),
+        # JWT expiration time (10 minute maximum)
+        'exp': int(expiration_time.timestamp()),
+        # Integration's GitHub identifier
+        'iss': app.integration_id
+    }
+
+    return jwt.encode(payload, app.private_key.encode('ascii'), algorithm='RS256')
+
+
+def request_access_token(installation):
+
+    print(get_jwt())
+
+    headers = {}
+    # note, won't work if netrc file is present
+    headers['Authorization'] = 'Bearer {0}'.format(get_jwt().decode('ascii'))
+    headers['Accept'] = 'application/vnd.github.machine-man-preview+json'
+
+    url = 'https://api.github.com/installations/{0}/access_tokens'.format(installation)
+
+    req = requests.post(url, headers=headers)
+
+    resp = req.json()
+
+    return resp['token']
+
+
 @app.route("/hook", methods=['POST'])
 def hook():
 
     if request.headers['X-GitHub-Event'] != 'pull_request':
         return "Not a pull_request event"
 
-    gh = Github(client_id=app.config["GITHUB_OAUTH_CLIENT_ID"],
-                client_secret=app.config["GITHUB_OAUTH_CLIENT_SECRET"])
-
     data = json.loads(request.data)
 
-    repo = gh.get_repo(data['repository']['full_name'])
+    token = request_access_token(data['installation'])
 
-    pr = repo.get_pull(int(data['number']))
+    headers = {}
+    headers['Authorization'] = 'token {0}'.format(token)
+    headers['Accept'] = 'application/vnd.github.machine-man-preview+json'
 
-    commit = repo.get_commit(pr.head.sha)
+    url_review = data['review_comments_url'].replace('comments', 'reviews')
 
-    # can also set target_url
-    commit.create_status('error',
-                         description='Your changelog sucks',
-                         context='astrochangebot')
+    data = {}
+    data['commit_id'] = data['head']['sha']
+    data['body'] = 'yay'
+    data['event'] = 'APPROVE'
+
+    requests.post(url_review, json=data, headers=headers)
+
+    url_status = data['statuses_url']
+
+    data = {}
+    data['state'] = 'error'
+    data['description'] = 'see latest review by astrochangebot'
+    data['context'] = 'astrochangebot'
+
+    requests.post(url_status, json=data, headers=headers)
+
+    changes_url = data['contents_url'].replace('{+path}', 'CHANGES.rst')
+
+    data = {}
+    data['ref'] = data['head']['ref']
+
+    requests.get(changes_url, json=data)
+    j = resp.json()
+
+    print(base64.b64decode(j['content']))
+
 
     return str(request.data)
-
 
 
 if __name__ == '__main__':
