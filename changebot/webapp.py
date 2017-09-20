8c6cf4822cb100eabbee761141e4f1ec8f6bf16b
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-09-19 23:50:01 -0400

    Move configuration for stale issues to environment variables

--- changebot/webapp.py
+++ changebot/webapp.py
@@ -14,6 +14,9 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 app.integration_id = int(os.environ['GITHUB_APP_INTEGRATION_ID'])
 app.private_key = os.environ['GITHUB_APP_PRIVATE_KEY']
 app.cron_token = os.environ['CRON_TOKEN']
+app.stale_issue_close = os.environ['STALE_ISSUE_CLOSE'].lower() == 'true'
+app.stale_issue_close_seconds = float(os.environ['STALE_ISSUE_CLOSE_SECONDS'])
+app.stale_issue_warn_seconds = float(os.environ['STALE_ISSUE_WARN_SECONDS'])
 
 
 @app.route("/")
