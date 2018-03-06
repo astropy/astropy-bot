732133ccc06b5ad1467b9b0cce0fc5bb9946a927
Stuart Mumford <stuart@cadair.com>
2018-03-06 10:37:03 +0000

    Add payload parseing

--- changebot/blueprints/circleci.py
+++ changebot/blueprints/circleci.py
@@ -16,31 +16,55 @@ def circleci_handler():
     if not request.data:
         print("No payload received")
 
-    payload = json.loads(request.data)
+    payload = json.loads(request.data)['payload']
 
     # Validate we have the keys we need, otherwise ignore the push
     required_keys = {'vcs_revision',
                      'username',
                      'reponame',
-                     'status'}
+                     'status',
+                     'build_num'}
 
-
-    print(payload)
-    if not required_keys.issubset(payload.keys()):
-        return 'Payload missing {}'.format(' '.join(required_keys - payload.keys()))
+    # if not required_keys.issubset(payload.keys()):
+    #     return 'Payload missing {}'.format(' '.join(required_keys - payload.keys()))
 
     # optional_keys = {}
     # invalid_keys = payload.keys() - (required_keys | optional_keys)
     # if invalid_keys:
     #     return f"Received unknown values {' '.join(invalid_keys)}."
 
-    if "target_url" not in payload:
-        payload['target_url'] = None
+    # if "target_url" not in payload:
+    #     payload['target_url'] = None
 
-
+    # if payload['status'] == 'success':
+    artifacts = get_artifacts_from_build(payload)
+    url = get_documentation_url_from_artifacts(artifacts)
+    print(url)
+    installation = 86691
+    if url:
+        set_commit_status(f"{payload['username']}/{payload['reponame']}", installation,
+                          payload['vcs_revision'], "success",
+                          "Click details to preview the documentation build", url)
     # set_commit_status(**payload)
 
     return "All good"
+
+
+def get_artifacts_from_build(p):
+    base_url = "https://circleci.com/api/v1.1"
+    query_url = f"{base_url}/project/github/{p['username']}/{p['reponame']}/{p['build_num']}/artifacts"
+    print(query_url)
+    response = requests.get(query_url)
+    print(response)
+    assert response.ok, response.content
+    return response.json()
+
+
+def get_documentation_url_from_artifacts(artifacts):
+    for artifact in artifacts:
+        # Find the root sphinx index.html
+        if "html/index.html" in artifact['path']:
+            return artifact['url']
 
 
 def set_status(repository, commit_hash, state, description, context, *, headers, target_url=None):
@@ -80,7 +104,5 @@ def set_commit_status(repository, installation, commit_hash, state, description,
 
     headers = github_request_headers(installation)
 
-    repo = RepoHandler(repository, 'master', installation)
-    allowed_url_regex = repo.get_config_value('commit_status_url_regex', '*')
-
-    print(repository, installation, commit_hash)
+    set_status(repository, commit_hash, state, description, "doc-preview-url",
+               headers=headers, target_url=target_url)
