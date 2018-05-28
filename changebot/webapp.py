13651fbe249a54b372a2086cd5dbbe6fc037e7d3
Stuart Mumford <stuart@cadair.com>
2018-05-28 16:54:52 +0100

    First pass at extensible PR checks

--- changebot/webapp.py
+++ changebot/webapp.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 
@@ -21,6 +22,39 @@ app.stale_issue_warn_seconds = float(os.environ['STALE_ISSUE_WARN_SECONDS'])
 app.stale_pull_requests_close = os.environ['STALE_PULL_REQUEST_CLOSE'].lower() == 'true'
 app.stale_pull_requests_close_seconds = float(os.environ['STALE_PULL_REQUEST_CLOSE_SECONDS'])
 app.stale_pull_requests_warn_seconds = float(os.environ['STALE_PULL_REQUEST_WARN_SECONDS'])
+
+
+"""
+Configuration for the pull request checker.
+"""
+
+app.pull_request_prolog = re.sub('(\w+)\n', r'\1', """
+Hi there @{user} :wave: - thanks for the pull request! I'm just
+ a friendly :robot: that checks for
+ issues related to the changelog and making sure that this
+ pull request is milestoned and labeled correctly. This is
+ mainly intended for the maintainers, so if you are not
+ a maintainer you can ignore this, and a maintainer will let
+ you know if any action is required on your part :smiley:.
+""").strip() + os.linesep + os.linesep
+
+
+app.pull_request_epilog = os.linesep + os.linesep + re.sub('(\w+)\n', r'\1', """
+*If there are any issues with this message, please report them
+ [here](https://github.com/astropy/astropy-bot/issues).*
+""").strip()
+
+# This should be a substring of either the prolog or the epilog which is used
+# to detect previous comments by the bot on the PR
+# TODO: Make this automatically determined based on the prolog
+app.pull_request_substring = "issues related to the changelog"
+
+app.pull_request_passed = 'All checks passed'
+app.bot_username = 'astropy-bot'
+app.pull_request_failed = f'There were failures in checks - see comments by @{app.bot_username} above'
+
+# Import this here to register the check with the pull request checker
+from changebot.blueprints.changelog_checker import check_changelog_consistency
 
 app.register_blueprint(pull_request_checker)
 app.register_blueprint(stale_issues)
