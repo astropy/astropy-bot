2951b688c9899aec5a3ef95519e0addc382eb0b8
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-06-20 12:25:01 -0400

    fix test

--- astropy_bot/tests/test_autolabel.py
+++ astropy_bot/tests/test_autolabel.py
@@ -11,14 +11,12 @@ class TestAutolabel:
 
     def setup_method(self, method):
 
-        self.files = [
-            {'filename': 'CHANGES.rst'},
-            {'filename': 'astropy/io/fits/convenience.py'},
-            {'filename': 'astropy/io/fits/fitstime.py'},
-            {'filename': 'astropy/io/fits/tests/test_fitstime.py'},
-            {'filename': 'astropy/coordinates/baseframe.py'},
-            {'filename': 'astropy/visualization/wcsaxes/coordinate_helpers.py'}
-        ]
+        self.files = ['CHANGES.rst', 'astropy/io/fits/convenience.py',
+                      'astropy/io/fits/fitstime.py',
+                      'astropy/io/fits/tests/test_fitstime.py',
+                      'astropy/coordinates/baseframe.py',
+                      'astropy/visualization/wcsaxes/coordinate_helpers.py']
+
         self.all_labels = ['analytic_functions', 'config', 'constants',
                            'convolution', 'coordinates', 'cosmology',
                            'io.ascii', 'io.fits', 'io.misc', 'io.misc.asdf',
@@ -33,9 +31,9 @@ class TestAutolabel:
         self.pr_handler.labels = ['Docs']
         self.pr_handler.milestone = None
         self.pr_handler.set_labels = self.set_labels
+        self.pr_handler.get_modified_files = self.get_modified_files
 
         self.repo_handler = MagicMock()
-        self.repo_handler.get_modified_files = self.get_modified_files
         self.repo_handler.get_all_labels = self.get_all_labels
 
     # Patched methods:
@@ -57,6 +55,6 @@ class TestAutolabel:
         assert 'Docs' in self.pr_handler.labels
 
         # now check that all the subpackage labels are added:
-        assert 'io.fits' in self.pr_handler.labels
-        assert 'coordinates' in self.pr_handler.labels
-        assert 'visualization.wcsaxes' in self.pr_handler.labels
+        assert 'io.fits' in self.labels
+        assert 'coordinates' in self.labels
+        assert 'visualization.wcsaxes' in self.labels
